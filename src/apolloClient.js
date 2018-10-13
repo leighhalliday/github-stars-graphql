38e064d8a72650c83d2782efac4128b93ba454a7
Leigh Halliday <leighhalliday@gmail.com>
2018-10-13 16:43:18 -0400

    Switching from Apollo Boost to using Apollo Client and configuring links

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -1,14 +1,53 @@
-import ApolloClient from "apollo-boost";
+// import ApolloClient from "apollo-boost";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { ApolloClient } from "apollo-client";
+import { ApolloLink } from "apollo-link";
+import { HttpLink } from "apollo-link-http";
+import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
+
+// const client = new ApolloClient({
+//   uri: "https://api.github.com/graphql",
+//   request: operation => {
+//     operation.setContext({
+//       headers: {
+//         Authorization: `bearer ${sessionStorage.getItem("token")}`
+//       }
+//     });
+//   }
+// });
+
+const cache = new InMemoryCache();
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({ message, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Path: ${path}`)
+    );
+  }
+
+  if (networkError) {
+    console.log(
+      `[Network error ${operation.operationName}]: ${networkError.message}`
+    );
+  }
+});
+
+const authLink = setContext((_, { headers }) => {
+  const context = {
+    headers: {
+      ...headers,
+      Authorization: `bearer ${sessionStorage.getItem("token")}`
+    }
+  };
+  return context;
+});
+
+const httpLink = new HttpLink({ uri: "https://api.github.com/graphql" });
 
 const client = new ApolloClient({
-  uri: "https://api.github.com/graphql",
-  request: operation => {
-    operation.setContext({
-      headers: {
-        Authorization: `bearer ${sessionStorage.getItem("token")}`
-      }
-    });
-  }
+  link: ApolloLink.from([errorLink, authLink, httpLink]),
+  cache
 });
 
 export default client;
